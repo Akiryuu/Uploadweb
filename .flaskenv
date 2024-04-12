62f742e74681476bddec2ff06f79441f235e0e99
akiryuu <foundy@post.cz>
2024-04-12 16:17:54 +0200

    Upload

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP=webface
+FLASK_ENV=development
+FLASK_DEBUG=True
---
+++ webface.py
@@ -0,0 +1,201 @@
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    session,
+    flash,
+    send_from_directory
+)
+import functools
+import string
+import random
+import datetime
+from sqlitewrap import SQLite
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+from sqlite3 import IntegrityError
+import os
+import uuid
+import base64
+
+# from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = b"totoj e zceLa n@@@hodny retezec nejlep os.urandom(24)"
+app.secret_key = b"x6\x87j@\xd3\x88\x0e8\xe8pM\x13\r\xafa\x8b\xdbp\x8a\x1f\xd41\xb8"
+
+
+def prihlasit(function):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        if "user" in session:
+            return function(*args, **kwargs)
+        else:
+            return redirect(url_for("login", url=request.path))
+
+    return wrapper
+
+
+@app.route("/", methods=["GET"])
+def base():
+    return render_template("base.html")
+
+
+@app.route('/login/', methods=["GET"])
+def login():
+    if 'user' not in session:
+        return render_template('login.html')
+    return render_template('logout.html')
+
+
+@app.route('/login/', methods=["POST"])
+def login_post():
+    username = request.form.get('username','')
+    password = request.form.get('password','')
+    url = request.args.get('url', '')
+    with SQLite('data.sqlite') as cur:
+        response = cur.execute('SELECT username, password FROM user WHERE username = ?',[username]).fetchone()
+        if response:
+            username, pass_hash = response
+            if check_password_hash(pass_hash, password):
+                session['user'] = username
+                flash('login successful', 'success')
+                if url:
+                    return redirect(url)
+                else:
+                    return redirect(url_for('base'))
+        flash('incorrect username or password', 'error')
+        return redirect(url_for('login', url=url))
+                
+
+@app.route('/logout/')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('base'))
+
+
+@app.route('/register/', methods=['POST'])
+def register_post():
+    username = request.form.get('username', '')
+    password_0 = request.form.get('password_0', '')
+    password_1 = request.form.get('password_1', '')
+    if password_0 != password_1:
+        flash('passwords do not match', 'error')
+        return redirect('register')        
+    pass_hash = generate_password_hash(password_0)
+    try:
+        with SQLite('data.sqlite') as cur:
+            cur.execute('INSERT into user (username, password) VALUES (?, ?)', [username, pass_hash])
+        flash(f'user {username} was successfully registered', 'success')
+    except IntegrityError:
+        flash(f'user {username} already exists', 'error')
+    return redirect(url_for('register'))
+
+
+@app.route('/register/', methods=['GET'])
+def register():
+    return render_template("register.html")
+
+@app.route("/vzkazy/", methods=["GET"])
+def vzkazy():
+    if "user" not in session:
+        flash("pro vstup se musíš přihlásit")
+        return redirect(url_for("login", url=request.path))
+    
+    with SQLite('data.sqlite') as cur:
+        response = cur.execute('SELECT username, body, datetime, message.id FROM user JOIN message ON user.id = message.user_id ORDER BY datetime DESC').fetchall()
+
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    return render_template("vzkazy.html", response=response, current_time=current_time)
+
+
+@app.route("/vzkazy/", methods=["POST"])
+def vzkazy_post():
+    if "user" not in session:
+        flash("pro vstup se musíš přihlásit")
+        return redirect(url_for("login", url=request.path))
+    
+    with SQLite('data.sqlite') as cur:
+        user_id = list(cur.execute('SELECT id FROM user WHERE username = ?', [session['user']]).fetchone())[0]
+
+    vzkaz = request.form.get('vzkaz')
+    if vzkaz:
+        with SQLite('data.sqlite') as cur:
+            cur.execute(
+                'INSERT INTO message (user_id, body, datetime) VALUES (?,?,?)', [user_id, vzkaz, datetime.datetime.now()],
+            )
+
+    return redirect(url_for("vzkazy"))
+
+@app.route('/vzkaz/vymazat', methods=['POST'])
+def vymaz_vzkaz():
+    if request.form.get('id'):
+        with SQLite('data.sqlite') as cur:
+            cur.execute('DELETE FROM message where id = ? and user_id = ?', [request.form.get('id'), cur.execute('SELECT id FROM user WHERE username = ?', [session['user']]).fetchone()[0]])
+    return redirect(url_for('vzkazy'))
+
+@app.route('/editovat/<int:_id>')
+@prihlasit
+def editovat(_id):
+    with SQLite('data.sqlite') as cur:
+        body = cur.execute('select body from message where id = ?', [_id]).fetchone()[0]
+    return render_template('editovat.html', body=body)
+
+@app.route('/editovat/<int:_id>', methods=['POST'])
+@prihlasit
+def editovat_post(_id):
+    if request.form.get('vzkaz'):
+        with SQLite('data.sqlite') as cur:
+            cur.execute('update message set body = ? where id = ? and user_id = (SELECT id FROM user WHERE username = ?)', [request.form.get('vzkaz') , _id, session['user']])
+    return redirect(url_for('vzkazy'))
+
+@app.route('/upload/', methods=['GET'])
+def upload():
+    with SQLite('data.sqlite') as cur:
+        images = []
+        for filename_origin, data in cur.execute('SELECT filename_origin, data FROM upload').fetchall():
+            images.append([filename_origin, base64.b64encode(data).decode('ascii'), f'image/{os.path.splitext(filename_origin)[-1][1:]}'])
+    return render_template('upload.html', response=images)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+uploadfolder = os.path.dirname(__file__)+'/files'
+allowed_extensions = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
+
+@app.route('/upload/', methods=['POST'])
+def upload_post():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename) and file.filename:
+        filename = secure_filename(file.filename)
+        filename_db = uuid.uuid1().hex+'-'+filename
+        file.save(os.path.join(uploadfolder, uuid.uuid1().hex+'-'+filename))
+        file.stream.seek(0)
+        with SQLite('data.sqlite') as cur:
+            cur.execute('INSERT INTO upload (filename_origin, filename_on_disk, data) VALUES (?, ?, ?)', (filename_db, filename, file.stream.read()))
+        flash('upload succesfull')
+        return redirect(url_for('upload'))
+    
+    flash('failed to upload file', 'error')
+    return redirect(url_for('upload'))
+
+@app.route('/upload2/', methods=['GET'])
+def upload2():
+    files = os.listdir(uploadfolder)
+    print(files)
+    return render_template('upload2.html', files=files)
+
+@app.route('/files/<path:filename>')
+def files(filename):
+    return send_from_directory(uploadfolder, filename)
